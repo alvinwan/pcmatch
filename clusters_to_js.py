fixed icp and eval, translation/rotation work

clusters_to_js.py
@@ -6,6 +6,7 @@ Usage:
     convert.py dir <raw_dir> <label_dir> [options]
 
 Options:
+    --transform     Whether or not
     --out=<out>     Directory for output [default: ./data/js]
     --variable=<v>  Name of variable to assign all data [default: data]
 """
@@ -69,10 +70,10 @@ def write_cluster_to_js(raw_path: str, label_path: str, out_path: str, variable:
     for path, pc, label in zip(paths, pcs, labels):
         obj_name = os.path.basename(path).replace('.npy', '').replace('.stl', '')
         if label_path is not None:
+            M = np.ones((4, pc.shape[0]))
+            M[:3, :] = pc.T
             T = label[2: 18].reshape((4, 4))
-            t = T[:3, 3]
-            R = T[:3, :3]
-            pc = (pc - pc.mean(axis=0) - t).dot(R)
+            pc = T.dot(M)[:3, :].T
         data[obj_name] = {
             'vertices': [{'x': x, 'y': y, 'z': z} for x, y, z in pc]}
         if label_path is not None:

label.py
@@ -29,12 +29,12 @@ def label(templates: np.array, samples: np.array) -> np.array:
     """Use ICP to classify all samples."""
     labels = None
     for sample in samples:
-        results = [icp(sample, template, max_iterations=1) for template in templates]
+        results = [icp(sample, template) for template in templates]
         distances = [np.sum(distance) for _, distance in results]
 
         i = int(np.argmin(distances))
         T, _ = results[i]  # T (4x4 homogenous transformation)
-        label = np.hstack((i, distances[i], np.ravel(T))).reshape((1, -1))
+        label = np.hstack((i, distances[i], np.ravel(T), distances)).reshape((1, -1))
         labels = label if labels is None else np.vstack((labels, label))
     if labels is None:
         raise UserWarning('No samples found.')